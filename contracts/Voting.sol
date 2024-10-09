1f7064e55484a4cec479bc0dd960c5711cbd5a28
Belin27000 <y.lecerf@protonmail.com>
2024-10-09 10:36:43 +0200

    FEAT AddProposal creation

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -21,7 +21,9 @@ contract Voting is Ownable{
     }
 
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
-
+    
+    //L'administrateur change la phase du processus de vente
+    
     function nextVotingStatus() public onlyOwner{ //Definis le status à l'etape suivante
         WorkflowStatus previousStatus=state; //stock la valeur de status avant modification dans previousStatus
 
@@ -37,6 +39,8 @@ contract Voting is Ownable{
     mapping (address => bool) whitelist; //Configuration de la liste blanche des address authorisée à voter
     event VoterRegistered(address voterAddress);
 
+    //L'administrateur enregistre les adresse Ethereum sur liste blanche
+
     function _whitelist(address _address) public onlyOwner{
         require(state == WorkflowStatus.RegisteringVoters,"Address can be added only in Registration phase"); //Check que nous sommes bien dans la phase d'enregistrement des adresses
         require(!whitelist[_address],"Address already registered");//Check si l'addresse n'est pas déjà dans la whitelist
@@ -44,6 +48,38 @@ contract Voting is Ownable{
 
         emit VoterRegistered(_address); //emet sur la blockchain l'adresse whitelisted
     }
-    
-    
+
+    /* Les électeurs inscrits sont autorisés à enregistrer leurs propositions 
+    pendant que la session d'enregistrement est active. */
+
+    struct Proposal { string description; uint voteCount; }
+    Proposal[] public proposals;
+
+    function _addProposal (string memory _description) external {
+        require(state == WorkflowStatus.ProposalsRegistrationStarted,"Proposal can be addes only in Proposal Registration phase"); //Check que nous sommes bien dans la phase d'enregistrement des adresses
+        require(whitelist[msg.sender],"Your adress is not whitelisted");
+
+        proposals.push(Proposal({
+            description: _description,
+            voteCount:0
+
+        }));
+
+    }
+    function getProposalCount() external view returns (uint){
+        return proposals.length;
+    }
+
+/* L'administrateur de vote met fin à la session d'enregistrement des propositions.
+
+L'administrateur du vote commence la session de vote.
+
+Les électeurs inscrits votent pour leur proposition préférée.
+
+L'administrateur du vote met fin à la session de vote.
+
+L'administrateur du vote comptabilise les votes.
+
+Tout le monde peut vérifier les derniers détails de la proposition gagnante.
+     */
 }
